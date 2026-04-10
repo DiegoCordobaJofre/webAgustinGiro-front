0149dd3b0b1c998cf98a9c1e0aa862b932e7fff1
diegocordobajofre <diegocordobajofre@gmail.com>
2026-04-10 13:41:26 +0200

    correct ux

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://webagustingiro-back.onrender.com/api',
-  /** Número de WhatsApp en formato internacional sin + ni espacios */
+  apiUrl: '  https://webagustingiro-back.onrender.com',
   whatsappNumber: '351927688101'
 };
-
-
-
-
-
-
-
-
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8050/api',
-  /** Número de WhatsApp en formato internacional sin + ni espacios (ej: 351927688101) */
   whatsappNumber: '351927688101'
 };
 
